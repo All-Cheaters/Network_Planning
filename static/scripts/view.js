fed6c5c9dbd40f1d46d93be776c86a2a4019b8ee
hr99 <1771012486@qq.com>
2019-12-18 22:22:53 +0800

    tinychange

--- static/scripts/view.js
+++ static/scripts/view.js
@@ -17,14 +17,13 @@ function addEle(name, pre_item, suc_item, last_time, earliest_start_time, earlie
     }
     $("#table_body").children("table").children("tbody").append($row);
     if (is_key == true) {
-        $row.css('backgroundColor', 'rgba(255, 127, 80, 0.5)');
+        $row.css('backgroundColor', 'rgba(159, 189, 255, 0.52)');
     }
 }
 
 
 function onSearch(searchContent) {
     let search = searchContent;
-    console.log(search);
     $("table").find("tbody tr").each(function () {
             let tableTr = $(this);
             let TrValue = tableTr.children("td:first").text();
@@ -53,7 +52,7 @@ $(function () {
         dataType: 'json', // 注意：这里是指希望服务端返回json格式的数据
         success: function (items) { // 这里的data就是json格式的数据
             items.forEach(function (v) {
-                addEle(v.item_name, v.item_pre_item, v.item_suf_item, v.item_last_time, v.item_earliest_start_time, v.item_earliest_finish_time, v.item_latest_start_time, v.item_latest_finish_time, v.item_free_time_difference, v.item_total_time_difference, v.is_key);
+                addEle(v.item_name, v.item_pre_item, v.item_suf_item, v.item_last_time, v.item_earliest_start_time, v.item_earliest_finish_time, v.item_latest_start_time, v.item_latest_finish_time, v.item_free_time_difference, v.item_total_time_difference, v.item_is_key);
             });
             $('#search').click(function () {
                 let searchname = $('#search_in').val();
@@ -61,7 +60,7 @@ $(function () {
             });
         },
         error: function (xhr, type) {
-            console.log(parseInt(getQueryString('project_id')))
+            // console.log(parseInt(getQueryString('project_id')))
             alert('Item数据获取异常')
         }
     });
